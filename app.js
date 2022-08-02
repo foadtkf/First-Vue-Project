3b9a664ed9a1202672ca20b41921d90faf43a1e4
foadtkf <fakhrulif@gmail.com>
2022-08-02 22:34:54 +0600

    executed first code with vue

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-const btn = document.querySelector("button");
-const ip = document.querySelector("input");
-const ul = document.querySelector("ul");
-function handleclick() {
-  const li = document.createElement("li");
-  li.textContent = ip.value;
-  ul.appendChild(li);
-  ip.value=''
-}
-btn.addEventListener("click", handleclick);
+// const btn = document.querySelector("button");
+// const ip = document.querySelector("input");
+// const ul = document.querySelector("ul");
+// function handleclick() {
+//   const li = document.createElement("li");
+//   li.textContent = ip.value;
+//   ul.appendChild(li);
+//   ip.value=''
+// }
+// btn.addEventListener("click", handleclick);
+Vue.createApp({
+data(){
+    return{
+        goals:[],
+        enteredValue:''
+    }
+    
+},
+methods:{
+    addGoal(){
+     this.goals.push(this.enteredValue)
+     this.enteredValue=''
+    }
+ }
+}).mount('#app')
